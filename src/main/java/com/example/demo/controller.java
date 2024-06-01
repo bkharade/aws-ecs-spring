865a61496aad85943a2149c6011fa99d80a83559
kharade <kharade@kharades-MBP.fios-router.home>
2024-06-01 19:47:49 -0400

    buildspec and dockerfile added

--- src/main/java/com/example/demo/controller.java
+++ src/main/java/com/example/demo/controller.java
@@ -1,14 +1,21 @@
 package com.example.demo;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/demo")
 public class controller {
 
-	@RequestMapping("/Spring")
+	@GetMapping("Spring")
 	public String index() {
 		return "Greetings from Spring Boot!";
+	}
+	
+	@GetMapping("iAmLive")
+	public String iamAlive() {
+		return "Installed at June 1st,2024";
 	}
 
 }
